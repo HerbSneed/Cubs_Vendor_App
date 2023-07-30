fba87975740654d697478d0456058e4d91a174bf
Herb Sneed <hmsneed@gmail.com>
2023-07-30 17:05:28 -0400

    html and styling on home page

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './views/**/*.handlebars'
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'cubblue': '#1B2D53',
+      'cubred': '#CF102B',
+      'white': '#FFFFFF',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
